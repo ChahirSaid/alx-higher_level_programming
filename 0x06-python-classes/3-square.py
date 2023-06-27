edabfdd9183a7b723a13bfe00fccbcfec6439e09
ChahirSaid <80861946+ChahirSaid@users.noreply.github.com>
2023-06-27 18:12:26 +0100

    Update 3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-class Square:
-    __size = None
+"""Task"""
 
+
+class Square:
+    """This is a class"""
     def __init__(self, size=0):
-        self.__size = size
-        if type(size) != int:
-            raise Exception("size must be an integer")
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
         if size < 0:
-            raise Exception("size must be >= 0")
+            raise ValueError("size must be >= 0")
+        self.__size = size
 
     def area(self):
-        return (self.__size * self.__size)
+        """Return the current square"""
+        return self.__size ** 2
