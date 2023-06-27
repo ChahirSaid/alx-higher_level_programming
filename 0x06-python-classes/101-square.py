ed51fc189f034567d00b27aef63877f42867d687
ChahirSaid <80861946+ChahirSaid@users.noreply.github.com>
2023-06-27 18:11:32 +0100

    Update 101-square.py

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -1,19 +1,26 @@
 #!/usr/bin/python3
+"""Task"""
+
+
 class Square:
+    """This is a class"""
     def __init__(self, size=0, position=(0, 0)):
         self.size = size
         self.position = position
 
     @property
     def size(self):
+        """Getter for size"""
         return self.__size
 
     @property
     def position(self):
+        """Getter for position"""
         return self.__position
 
     @position.setter
     def position(self, value):
+        """Setter for position"""
         if type(value) is not tuple or len(value) != 2 or \
                 type(value[0]) is not int or type(value[1]) is not int or \
                 value[0] < 0 or value[1] < 0:
@@ -22,6 +29,7 @@ class Square:
 
     @size.setter
     def size(self, value):
+        """Setter for size"""
         if type(value) is not int:
             raise TypeError("size must be an integer")
         if value < 0:
@@ -29,9 +37,11 @@ class Square:
         self.__size = value
 
     def area(self):
+        """Return the current square"""
         return self.__size ** 2
 
     def my_print(self):
+        """Print square using # taking into account position"""
         if not self.size:
             print()
             return
